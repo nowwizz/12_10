706a9adf4549b91d16f0420c39d21700de591af4
Yu jiwon <wi1767@naver.com>
2024-12-04 14:18:48 +0900

    cd and pwd commend is added

--- src/myshell.c
+++ src/myshell.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define MAX_LINE 80
 #define MAX_ARGS 10
@@ -35,7 +36,13 @@ int main(){
         if(strcmp(argv[0], "exit")==0){
             printf("Goodbye~\n");
             exit(0);
+        } else if (strcmp(argv[0], "cd")==0){
+            chdir(argv[1]);
+        } else if(strcmp(argv[0], "pwd")==0){
+            getcwd(input, MAX_LINE);
+            printf("%s\n", input);
         }
+        
 
     }
     return 0;
